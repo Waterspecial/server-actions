46a61ec9fa93aa699e29ad7893b45643edd400e0
Waterspecial <boluwatifeomirinde080@gmail.com>
2024-07-11 03:02:01 +0100

    nextjs server actions and data mutation

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   const url =
-    "mongodb+srv://boluwatifeomirinde080:FAzgQTjUpp7OsSAN@blog.li42ds6.mongodb.net/?retryWrites=true&w=majority&appName=Blog";
+    "mongodb+srv://boluwatifeomirinde080:<password>@blog.li42ds6.mongodb.net/?retryWrites=true&w=majority&appName=Blog";
 
   mongoose
     .connect(url)
